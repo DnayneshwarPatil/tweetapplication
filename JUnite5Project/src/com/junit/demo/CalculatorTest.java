8e1925c38bcbc405b58dd7bb0bdcf128672b0314
DnyaneshwarPatil <dnyanudethe.dd@gmail.com>
2022-11-10 15:17:25 +0530

    Junit testing assertEquals method

--- JUnite5Project/src/com/junit/demo/CalculatorTest.java
+++ JUnite5Project/src/com/junit/demo/CalculatorTest.java
@@ -1,32 +1,41 @@
 package com.junit.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
 	@Test
+	@DisplayName("Adding number")
 	public void testAddMethod() {
 
 		Calculator cl = new Calculator();
 		int sum = cl.add(10, 20);
-		System.out.println(sum);
+		assertEquals(30, sum);
+		System.out.println("Add Value :"+sum);
 
 	}
 
 	@Test
+	@DisplayName("Substracting two number")
 	public void testSubMethod() {
 
 		Calculator cl = new Calculator();
-		int sub = cl.add(50, 20);
+		int sub = cl.sub(50, 20);
+		assertEquals(30, sub);
 		System.out.println("Sub Value :" + sub);
 
 	}
 
 	@Test
+	@DisplayName("Multiplying two  number")
 	public void testProductMethod() {
 
 		Calculator cl = new Calculator();
-		int pro = cl.add(20, 20);
+		int pro = cl.product(20, 20);
+		assertEquals(400, pro);
 		System.out.println("Product Valu :" + pro);
 
 	}
