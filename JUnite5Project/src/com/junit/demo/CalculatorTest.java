6fbf4beaec7df053b31814301c96b00455aae6c5
DnyaneshwarPatil <dnyanudethe.dd@gmail.com>
2022-11-10 14:30:57 +0530

    Junit testing calculator add,sum,product

--- JUnite5Project/src/com/junit/demo/CalculatorTest.java
+++ JUnite5Project/src/com/junit/demo/CalculatorTest.java
@@ -1,14 +1,34 @@
 package com.junit.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAddMethod() {
+
+		Calculator cl = new Calculator();
+		int sum = cl.add(10, 20);
+		System.out.println(sum);
+
+	}
+
+	@Test
+	public void testSubMethod() {
+
+		Calculator cl = new Calculator();
+		int sub = cl.add(50, 20);
+		System.out.println("Sub Value :" + sub);
+
+	}
+
+	@Test
+	public void testProductMethod() {
+
+		Calculator cl = new Calculator();
+		int pro = cl.add(20, 20);
+		System.out.println("Product Valu :" + pro);
+
 	}
 
 }
